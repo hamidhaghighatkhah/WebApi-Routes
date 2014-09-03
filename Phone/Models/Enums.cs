1385d426e195a09aa991816c11629406539874d2
hamidhaghighatkhah <haghighat@mail.ru>
2014-09-03 10:04:49 +0430

    Flag Attribute on enums

---
+++ Phone/Models/Enums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Phone.Models
+{
+    [Flags]
+    public enum SimOperators
+    {
+        Irancell = 2,
+        Mci = 4,
+        Rightel = 8,
+        Taliya = 16
+    }
+    class program
+    {
+        static void main(string[] args)
+        {
+            int op = 14;
+            Console.WriteLine((SimOperators)op);
+        }
+    }
+}
